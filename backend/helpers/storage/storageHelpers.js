97c25ac3b28a2b41fb13413e243feafc6398ced0
Christiaan Seerden <christiaanseerden@live.nl>
2021-04-11 18:25:43 +0200

    Add proper imports to newly extracted helper file

--- backend/helpers/storage/storageHelpers.js
+++ backend/helpers/storage/storageHelpers.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+
 export function storeNinjaValueSnapshot(chaosValueEntry) {
     const filepath = path.join(path.resolve(), '/helpers/api/ninjaChaosValues.json');
     const ninjaChaosValues = JSON.parse(readFileSync(filepath));
