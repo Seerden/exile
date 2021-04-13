8b1414a04d79f8793f80bf422c64247549018551
Christiaan Seerden <christiaanseerden@live.nl>
2021-04-13 15:08:47 +0200

    Fix parseNinjaPage to properly loop over entries. Locally store a number of ninja pages with item name/icon

--- backend/dist/helpers/api/staticNinjaResults.js
+++ backend/dist/helpers/api/staticNinjaResults.js
@@ -1,21 +1,39 @@
-import { fetchNinjaPage } from '../api/ninjaApi';
-import { writeFileSync } from 'fs';
+import { itemObj } from '../api/ninjaPages.js';
+import { fetchNinjaPage } from '../api/ninjaApi.js';
+import { writeFileSync, openSync, closeSync } from 'fs';
 import path from 'path';
+;
 function parseNinjaPage(lines, fields) {
     // extract only the requested fields from the poe.ninja API response
-    return fields.reduce((acc, cur) => ({ ...acc, [cur]: lines[cur] }), {});
+    const result = [];
+    for (const [index, item] of lines.entries()) {
+        result.push(fields.reduce((acc, cur) => {
+            return { ...acc, [cur]: lines[index][cur] };
+        }, {}));
+    }
+    return result;
 }
 async function locallyStoreNinjaPage(league, itemType, fields) {
     try {
         const ninjaPage = await fetchNinjaPage(league, itemType);
         const parsedPage = parseNinjaPage(ninjaPage, fields);
-        const filepath = path.join(path.resolve(), `/helpers/api/staticNinjaPages/${itemType.kind}/${itemType.type}.json`);
         if (parsedPage) {
-            writeFileSync(filepath, JSON.stringify(parsedPage));
+            const filepath = path.join(path.resolve(), `backend/helpers/api/staticNinjaPages/${itemType.kind}/${itemType.type}.json`);
+            closeSync(openSync(filepath, 'w'));
+            const parsedPageWithMetaData = {
+                type: itemType.type,
+                date: new Date(),
+                items: parsedPage
+            };
+            writeFileSync(filepath, JSON.stringify(parsedPageWithMetaData));
+            console.log(`Saved response from poe.ninja for ${itemType.type} page`);
         }
     }
     catch (error) {
         console.error(error);
     }
+}
+for (const entry of itemObj) {
+    await locallyStoreNinjaPage('Ritual', { kind: 'Item', type: entry.type }, ['name', 'icon']);
 }
 //# sourceMappingURL=staticNinjaResults.js.map
--- backend/helpers/api/staticNinjaResults.ts
+++ backend/helpers/api/staticNinjaResults.ts
@@ -1,29 +1,63 @@
-import { itemObj, currencyObj, ItemTypeInterface } from '../api/ninjaPages';
-import { fetchNinjaPage } from '../api/ninjaApi';
-import { readFileSync, writeFileSync} from 'fs';
+import { itemObj, currencyObj, ItemTypeInterface } from '../api/ninjaPages.js';
+import { fetchNinjaPage } from '../api/ninjaApi.js';
+import { readFileSync, writeFileSync, openSync, closeSync, open, close } from 'fs';
 import path from 'path';
 
-interface ParsedNinjaResponse {
+type ParsedNinjaResponse = {
+    name?: string,
+    icon?: string,
     [key: string]: any
-}
+}[];
 
-type NinjaFields = string[]
+type NinjaFields = string[];
 
-function parseNinjaPage(lines, fields: NinjaFields): ParsedNinjaResponse {
+type NinjaResponse = Array<{ [key: string]: string | string[] | number[] }>;
+
+interface ParsedPageWithMetaData {
+    type: string,
+    date: Date,
+    items: ParsedNinjaResponse
+};
+
+function parseNinjaPage(lines: NinjaResponse, fields: NinjaFields): ParsedNinjaResponse {
     // extract only the requested fields from the poe.ninja API response
-    return fields.reduce((acc, cur) => ({...acc, [cur]: lines[cur]}), {})
+    const result: Object[] = [];
+
+    for (const [index, item] of lines.entries()) {
+        result.push(fields.reduce((acc, cur) => {
+            return { ...acc, [cur]: lines[index][cur] }
+        }, {}))
+    }
+
+    return result;
 }
 
 async function locallyStoreNinjaPage(league: string, itemType: ItemTypeInterface, fields: NinjaFields): Promise<void> {
     try {
         const ninjaPage = await fetchNinjaPage(league, itemType)
         const parsedPage = parseNinjaPage(ninjaPage, fields)
-        const filepath = path.join(path.resolve(), `/helpers/api/staticNinjaPages/${itemType.kind}/${itemType.type}.json`);
-    
+
         if (parsedPage) {
-            writeFileSync(filepath, JSON.stringify(parsedPage));
+            const filepath = path.join(path.resolve(), `backend/helpers/api/staticNinjaPages/${itemType.kind}/${itemType.type}.json`);
+
+            closeSync(openSync(filepath, 'w'));
+
+            const parsedPageWithMetaData: ParsedPageWithMetaData = {
+                type: itemType.type,
+                date: new Date(),
+                items: parsedPage
+            }
+
+            writeFileSync(filepath, JSON.stringify(parsedPageWithMetaData));
+            console.log(`Saved response from poe.ninja for ${itemType.type} page`);
         }
     } catch (error) {
         console.error(error);
+    }
+}
+
+async function locallyStoreAllNinjaItemPages() {
+    for (const entry of itemObj) {
+        await locallyStoreNinjaPage('Ritual', { kind: 'Item', type: entry.type }, ['name', 'icon'])
     }
 }
