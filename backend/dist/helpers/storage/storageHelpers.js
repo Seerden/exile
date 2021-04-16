bd0a8ee437cd46b329d3a7d63900659638e62be2
Christiaan Seerden <christiaanseerden@live.nl>
2021-04-16 15:39:36 +0200

    Untrack backend/dist

--- backend/dist/helpers/storage/storageHelpers.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { readFileSync, writeFileSync } from 'fs';
-export function storeNinjaValueSnapshot(chaosValueEntry) {
-    const filepath = path.join(path.resolve(), '/helpers/api/ninjaChaosValues.json');
-    const ninjaChaosValues = JSON.parse(readFileSync(filepath).toString());
-    ninjaChaosValues.push(chaosValueEntry);
-    writeFileSync(filepath, JSON.stringify(ninjaChaosValues));
-}
-//# sourceMappingURL=storageHelpers.js.map
